b7a273c646eb86d7ca86bafa7c80c000872a0bd6
aulaskar <ismo.aulaskari@helsinki.fi>
2025-01-04 13:31:32 +0200

    planning logic handling

--- src/main/java/fi/aulaskari/ismo/simplehomeautomation/service/CheckStateProcessor.java
+++ src/main/java/fi/aulaskari/ismo/simplehomeautomation/service/CheckStateProcessor.java
@@ -4,6 +4,8 @@ import fi.aulaskari.ismo.simplehomeautomation.model.Configuration;
 import fi.aulaskari.ismo.simplehomeautomation.model.Fact;
 import org.apache.camel.Exchange;
 
+import java.util.Date;
+
 public class CheckStateProcessor implements org.apache.camel.Processor {
 
     private final Configuration conf;
@@ -16,14 +18,23 @@ public class CheckStateProcessor implements org.apache.camel.Processor {
     public void process(Exchange exchange) throws Exception {
         //athome, homelocked, daylight
 
+
         Fact alarm = conf.getAlarm();
         if (alarm.needsHandling()) {
 //do sth with producertemplate ?
+            //siren? Cellphone?
         }
 
         Fact alert = conf.getAlert();
         if (alert.needsHandling()) {
-//do sth with producertemplate ?
+            if (conf.getHomeLocked().isActive()) {
+                Fact newAlarm = new Fact("alarm");
+                conf.setAlarm(newAlarm);
+//do sth with newAlarmproducertemplate?
+                //notification to phone?
+                alert.setForwarded(new Date());
+            }
+
         }
 
         Fact movementOutside = conf.getMovementOutside();
